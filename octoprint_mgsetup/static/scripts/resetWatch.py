0d14b6908813e0c0cc8f2abf5442bca098fbcfd6
Kyle Granat <kyle.granat@gmail.com>
2017-05-27 13:46:17 -0400

    Updated script for resetting defualts

--- octoprint_mgsetup/static/scripts/resetWatch.py
+++ octoprint_mgsetup/static/scripts/resetWatch.py
@@ -8,11 +8,12 @@ import serial
 GPIO.setwarnings(False)
 
 buttonPin    = 4
-holdTime     = 5     # Duration for button hold (shutdown)
+rebootHoldTime     = 5     # Duration for button hold (shutdown)
+resetPasswordsHoldTime     = 55     # Duration for button hold (shutdown)
 tapTime      = .1  # Debounce time for button taps
 nextInterval = 0.0   # Time of next recurring operation
 
-print("test1")
+#print("test1")
 
 #subprocess.call(["echo "M300 S110 P100" > /dev/ttyS0""])
 
@@ -29,13 +30,15 @@ print("test1")
 
 # Called when button is briefly tapped.  Invokes time/temperature script.
 def tap():
-  print("test")
+  pass
+  #print("test")
   #subprocess.call(["python", "timetemp.py"])
 
 
 # Called when button is held down.  Prints image, invokes shutdown process.
-def hold():
+def rebootHold():
   time.sleep(2)
+  #print("reboot")
 
   ser = serial.Serial("/dev/ttyS0", 115200, timeout=1)
   ser.close()
@@ -48,6 +51,20 @@ def hold():
 
   subprocess.call("sync")
   subprocess.call(["shutdown", "-h", "now"])
+
+
+
+def resetPasswordHold():
+  time.sleep(2)
+  #print("reset")
+  ser = serial.Serial("/dev/ttyS0", 115200, timeout=1)
+  ser.close()
+  ser.open()
+  ser.write("M300 S523 P400 \r\n")
+  ser.write("M300 S623 P400 \r\n")
+  ser.write("M300 S723 P400 \r\n")
+  ser.close()
+  #execute script here
 
 
 # Initialization
@@ -66,7 +83,8 @@ GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 prevButtonState = GPIO.input(buttonPin)
 prevTime        = time.time()
 tapEnable       = False
-holdEnable      = False
+rebootHoldEnable      = False
+resetPasswordHoldEnable      = False
 
 # Main loop
 while(True):
@@ -80,23 +98,37 @@ while(True):
     prevButtonState = buttonState   # Yes, save new state/time
     prevTime        = t
   else:                             # Button state unchanged
-    if (t - prevTime) >= holdTime:  # Button held more than 'holdTime'?
+    if (t - prevTime) >= rebootHoldTime:  # Button held more than 'rebootHoldTime'?
       # Yes it has.  Is the hold action as-yet untriggered?
-      if holdEnable == True:        # Yep!
+      if rebootHoldEnable == True:        # Yep!
+        #print("reboot1")
         while(GPIO.input(buttonPin) == False):
           pass
-        hold()                      # Perform hold action (usu. shutdown)
-        holdEnable = False          # 1 shot...don't repeat hold action
+          # t           = time.time()
+          # if (t - prevTime) >= resetPasswordsHoldTime:
+          #   print("resetInternal")
+
+        t           = time.time()
+        if (t - prevTime) >= resetPasswordsHoldTime:
+          #print("resetInternal")
+          resetPasswordHold()
+        else :   
+          rebootHold()                      # Perform hold action (usu. shutdown)
+
+        rebootHoldEnable = False          # 1 shot...don't repeat hold action
+        resetPasswordHoldEnable = False
         tapEnable  = False          # Don't do tap action on release
-    elif (t - prevTime) >= tapTime: # Not holdTime.  tapTime elapsed?
+    elif (t - prevTime) >= tapTime: # Not rebootHoldTime.  tapTime elapsed?
       # Yes.  Debounced press or release...
       if buttonState == True:       # Button released?
-        if tapEnable == True:       # Ignore if prior hold()
+        if tapEnable == True:       # Ignore if prior rebootHold()
           tap()                     # Tap triggered (button released)
           tapEnable  = False        # Disable tap and hold
-          holdEnable = False
+          rebootHoldEnable = False
+          resetPasswordHoldEnable = False
       else:                         # Button pressed
         tapEnable  = True           # Enable tap and hold actions
-        holdEnable = True
+        rebootHoldEnable = True
+        resetPasswordHoldEnable = True
 
  
