98cef17da344013f4cb0d8e717f21983817c5705
Kyle Granat <kyle.granat@gmail.com>
2017-05-30 17:41:17 -0400

    fixing upload script

--- octoprint_mgsetup/static/scripts/upload.sh
+++ octoprint_mgsetup/static/scripts/upload.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+cd /home/pi/m3firmware
+echo "M300 S659 P500" > /dev/ttyS0
 
 platformio run
+
+echo "M300 S659 P500" > /dev/ttyS0
+echo "M300 S859 P500" > /dev/ttyS0
 /home/pi/.platformio/packages/tool-avrdude/avrdude -v -cwiring -p atmega2560 -P/dev/ttyS0 -b115200 -D -Uflash:w:.pioenvs/megaatmega2560/firmware.hex
-
-
-
 
