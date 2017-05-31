779a9c5612b1391894288ff736e1466ae7b7f9a6
Josh <josh@makergear.com>
2017-05-31 19:59:38 +0000

    Added another echo to the end of upload.sh .

--- octoprint_mgsetup/static/scripts/upload.sh
+++ octoprint_mgsetup/static/scripts/upload.sh
@@ -8,4 +8,4 @@ platformio run
 echo "M300 S659 P500" > /dev/ttyS0
 echo "M300 S859 P500" > /dev/ttyS0
 /home/pi/.platformio/packages/tool-avrdude/avrdude -v -cwiring -p atmega2560 -P/dev/ttyS0 -b115200 -D -Uflash:w:.pioenvs/megaatmega2560/firmware.hex
-
+echo "Firmware uploaded!"
