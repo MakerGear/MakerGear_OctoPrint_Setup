f3a8e317b57cf19bc3397698249d3d8f9d568d88
Kyle Granat <kyle.granat@gmail.com>
2017-06-12 00:11:00 -0400

    patch patch patch

--- octoprint_mgsetup/__init__.py
+++ octoprint_mgsetup/__init__.py
@@ -401,8 +401,8 @@ class MGSetupPlugin(octoprint.plugin.StartupPlugin,
 			self.resetRegistration()
 			self._logger.info("Registration reset!")
 		elif action["action"] == 'patch':
-		 	self._execute("/home/pi/oprint/local/lib/python2.7/site-packages/octoprint_mgsetup/static/patch/patch.sh")
-		 	self._logger.info("Patch started.")
+			self._execute("/home/pi/oprint/local/lib/python2.7/site-packages/octoprint_mgsetup/static/patch/patch.sh")
+			self._logger.info("Patch started.")
 
 
 	def turnSshOn(self):
