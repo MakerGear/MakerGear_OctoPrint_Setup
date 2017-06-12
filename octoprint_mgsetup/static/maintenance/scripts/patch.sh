cfd8f381031f61ecb18df45aa2829f6f7a073942
Kyle Granat <kyle.granat@gmail.com>
2017-06-11 23:36:21 -0400

    permissions patch

---
+++ octoprint_mgsetup/static/maintenance/scripts/patch.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+sudo chown pi /home/pi/.octoprint/uploads/demos/stl/Bacteriophage.stl
+sudo chgrp pi /home/pi/.octoprint/uploads/demos/stl/Bacteriophage.stl
+
+
+sudo chown pi /home/pi/.octoprint/uploads/demos/stl/Flex_Rex.stl
+sudo chgrp pi /home/pi/.octoprint/uploads/demos/stl/Flex_Rex.stl
+
+
+sudo chown pi /home/pi/.octoprint/uploads/demos/pla/Bacteriophage_s3d.gcode
+sudo chgrp pi /home/pi/.octoprint/uploads/demos/pla/Bacteriophage_s3d.gcode
+
+
+sudo chown pi /home/pi/.octoprint/uploads/demos/pla/Flex_Rex_s3d.gcode
+sudo chgrp pi /home/pi/.octoprint/uploads/demos/pla/Flex_Rex_s3d.gcode
+
+
+
+
+sudo chown pi /home/pi/.octoprint/uploads/deletme
+sudo chgrp pi /home/pi/.octoprint/uploads/deletme
+
+sudo chown pi /home/pi/.octoprint/uploads/deletme/*.*
+sudo chgrp pi /home/pi/.octoprint/uploads/deletme/*.*
+
