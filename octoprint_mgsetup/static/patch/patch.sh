5326536739a1d9229e7a83b3d5ee427b94a8ce4e
Kyle Granat <kyle.granat@gmail.com>
2017-08-20 22:11:54 -0400

    log patches

--- octoprint_mgsetup/static/patch/patch.sh
+++ octoprint_mgsetup/static/patch/patch.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-sudo rm syslog.*.gz
-sudo rm shaproxy.log.*.gz
-sudo rm kern.log.*.gz
-sudo rm messages.*
-sudo rm netconnectd.log.*.gz
-sudo rm daemon.*
-sudo rm debu*
-sudo rm haproxy*
-sudo rm kern*
-sudo rm messages.*
-sudo rm netconnectd.log.*.gz 
-sudo rm netconnectd.log.old 
-sudo rm syslog.*.gz
+sudo rm /var/log/syslog.*.gz
+sudo rm /var/log/messages.*
+sudo rm /var/log/netconnectd.log.*.gz
+sudo rm /var/log/daemon.*
+sudo rm /var/log/debu*
+sudo rm /var/log/haproxy*
+sudo rm /var/log/kern*
+sudo rm /var/log/user*
+sudo rm /var/log/auth*
+sudo rm /var/log/alternatives*
+sudo rm /var/log/boot*
+sudo rm /var/log/dpkg*
+sudo rm /var/log/webcamd*
+sudo rm /var/log/map_iface*
+sudo rm /var/log/bootstrap*
+sudo rm /var/log/messages.*
+sudo rm /var/log/netconnectd.log.old 
 sudo truncate -s 0 /var/log/syslog
 sudo truncate -s 0 /var/log/syslog.1
 sudo truncate -s 0 /var/log/netconnectd.log
