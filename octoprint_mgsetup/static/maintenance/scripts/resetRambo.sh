fb8f18151512df62143478878e9aba35f1c976b8
Kyle Granat <kyle.granat@gmail.com>
2017-08-27 21:09:34 -0400

    commented out reset code

--- octoprint_mgsetup/static/maintenance/scripts/resetRambo.sh
+++ octoprint_mgsetup/static/maintenance/scripts/resetRambo.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+
 gpio -g mode 4 out
 gpio -g write 4 0
 gpio -g write 4 1
 gpio -g mode 4 out
 gpio -g mode 4 up
+
+#/home/pi/oprint/bin/octoprint client command "/api/connection" "disconnect"
+
+#gpio -g mode 4 out
+#gpio -g write 4 0
+#gpio -g write 4 1
+#gpio -g mode 4 out
+#gpio -g mode 4 up
+
+#/home/pi/oprint/bin/octoprint client command "/api/connection" "connect"
