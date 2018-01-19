d7f76264851208035cb2b520fb1a8cc2077da853
Kyle Granat <kyle@makergear.com>
2018-01-19 16:59:38 +0000

    fake news deletme

--- octoprint_mgsetup/static/js/hostname.js
+++ octoprint_mgsetup/static/js/hostname.js
@@ -1,18 +1,11 @@
-var hostName = "octodemom31";
-var ip = ["88", "89", "90"];
+var hostName = "M3Printer09P89";
+    var generate = 1516380018.6;
 
-
-for (i = 0; i < ip.length; i++) 
-{ 
-    
- id1 = ("ip"+ip[i].toString());
-
- hostDiv = document.getElementById(id1);
-
- hostDiv.innerHTML = hostName
-
-}
-
-
-;
-
+        var ip = ["156","-1"];
+        for (i = 0; i < ip.length; i++) 
+        { 
+        id1 = ("ip"+ip[i].toString());
+        hostDiv = document.getElementById(id1);
+        hostDiv.innerHTML = hostName;
+        }
+        
