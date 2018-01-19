eea48332dc5f1cede232965675146628fce73b6d
Kyle Granat <kyle@makergear.com>
2018-01-19 17:43:29 +0000

    hostname.js probably needs to be ignored

--- octoprint_mgsetup/static/js/hostname.js
+++ octoprint_mgsetup/static/js/hostname.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 var hostName = "M3Printer09P89";
-    var generate = 1516380018.6;
+    var generate = 1516383485.16;
 
         var ip = ["156","-1"];
-=======
-var hostName = "M3Printer51G90";
-    var generate = 1516223037.9;
-
-        var ip = ["139","-1"];
->>>>>>> 5393257abb812c1b2ab772c3e31a0a95c232aedf
         for (i = 0; i < ip.length; i++) 
         { 
         id1 = ("ip"+ip[i].toString());
