d99689384db7a30d35fca4641dd2fdad66a65b21
Kyle Granat <kyle@makergear.com>
2018-06-15 11:38:44 +0000

    more upload script patches + version change

--- octoprint_mgsetup/static/js/hostname.js
+++ octoprint_mgsetup/static/js/hostname.js
@@ -1,5 +1,5 @@
 var hostName = "M3Printer37K03";
-    var generate = 1528976426.55;
+    var generate = 1529057455.54;
 
         var ip = ["125","-1"];
         for (i = 0; i < ip.length; i++) 
--- octoprint_mgsetup/static/maintenance/scripts/upload.py
+++ octoprint_mgsetup/static/maintenance/scripts/upload.py
@@ -34,6 +34,7 @@ avrFlag = 0
 for line in os.popen("pgrep -x \"avrdude\""):
 	print ("Warning: AVRdude is already running, Quitting upload process. You may need to restart the printer to clear this warning.")
 	sys.stdout.flush()
+	quit()
    
 subprocess.check_output('echo $(date) > /home/pi/.octoprint/logs/firmware.log',shell=True)
 
@@ -60,7 +61,7 @@ os.chdir("/home/pi/m3firmware/")
 
 try:
 	subprocess.check_output('platformio run --target clean >> /home/pi/.octoprint/logs/firmware.log',shell=True)
-	print "Cleaned Complilation Build - Starting Compiling"
+	print "Cleaned Compilation Build - Starting Compiling"
 	sys.stdout.flush()
 except subprocess.CalledProcessError:
 	print "Failed to Clean directory, quitting"
@@ -94,7 +95,7 @@ print ("Storing previous firmware settings")
 subprocess.check_output('echo Storing  previous firmware settings >> /home/pi/.octoprint/logs/firmware.log',shell=True)
 sys.stdout.flush()
 
-ser = serial.Serial('/dev/ttyS0', 115200, timeout=2, xonxoff=False, rtscts=False, dsrdtr=False) #start serial port to upload script
+ser = serial.Serial('/dev/ttyS0', 115200, timeout=.5, xonxoff=False, rtscts=False, dsrdtr=False) #start serial port to upload script
 ser.flushInput()	#clear any latent input
 ser.flushOutput()   #clear any latent output
 
@@ -106,11 +107,14 @@ printerName = None
 extruderCount = None
 zprobe = None
 matcher = None
+zprobeMatcher = None
 
 ser.write("M115\n\r") #request firmware info
+print("m115")
 
 lineCount = 0	#keep track of how many lines we read
 while lineCount < 20 : #max out to 100 lines
+
 	data_raw = ser.readline() #read one serial line
 	lineCount = lineCount + 1 #incerease number of lines we've read
 
@@ -127,9 +131,8 @@ while lineCount < 20 : #max out to 100 lines
 		responseLine = data_raw
 
 #match to figure out version, machine type, and extruder count
-if matcher != None:
+if responseLineProbe != None:
 	matcher = re.match (r'FIRMWARE_NAME:Marlin ([0-9]\.[0-9]\.[0-9]\.?[0-9]?\.?[0-9]?) \(Github\) SOURCE_CODE_URL:https://github.com/MakerGear/m3firmware PROTOCOL_VERSION:1.0 MACHINE_TYPE:(.*) EXTRUDER_COUNT:([1-2])', responseLine)
-if zprobeMatcher != None:
 	zprobeMatcher = re.match (r'Cap:Z_PROBE:([0-1])', responseLineProbe)
 
 if (matcher == None) or (zprobeMatcher == None):
@@ -169,37 +172,38 @@ m851String = None
 
 
 lineCount = 0
-while lineCount < 100 : #max out to 100 lines
-	data_raw = ser.readline() #read one serial line
-	#print(data_raw)
-	lineCount = lineCount + 1
-	responseLine = data_raw
+if matcher != None:
+	while lineCount < 100 : #max out to 100 lines
+		data_raw = ser.readline() #read one serial line
+		#print(data_raw)
+		lineCount = lineCount + 1
+		responseLine = data_raw
 
-	#look if its a line we need
+		#look if its a line we need
 
-	if data_raw.startswith("ok") :
-		#print "done"
-		lineCount = 100
+		if data_raw.startswith("ok") :
+			#print "done"
+			lineCount = 100
 
-	elif data_raw.startswith("echo:  M206") :
-		matcher = re.match (r'echo:  M206 X(-?[0-9]{1,3}\.[0-9][0-9]) Y(-?[0-9]{1,3}\.[0-9][0-9]) Z(-?[0-9]{1,3}\.[0-9][0-9])', responseLine)
-		if matcher:
-			m206X = matcher.group(1) 
-			m206Y = matcher.group(2) 
-			m206Z = matcher.group(3) 
+		elif data_raw.startswith("echo:  M206") :
+			matcher = re.match (r'echo:  M206 X(-?[0-9]{1,3}\.[0-9][0-9]) Y(-?[0-9]{1,3}\.[0-9][0-9]) Z(-?[0-9]{1,3}\.[0-9][0-9])', responseLine)
+			if matcher:
+				m206X = matcher.group(1) 
+				m206Y = matcher.group(2) 
+				m206Z = matcher.group(3) 
 
-	elif data_raw.startswith("echo:  M218") :
-		matcher = re.match (r'echo:  M218 T1 X(-?[0-9]{1,3}\.[0-9][0-9]) Y(-?[0-9]{1,3}\.[0-9][0-9]) Z(-?[0-9]{1,3}\.[0-9][0-9])', responseLine)
-		if matcher:
-			m218X = matcher.group(1) 
-			m218Y = matcher.group(2) 
-			m218Z = matcher.group(3) 
+		elif data_raw.startswith("echo:  M218") :
+			matcher = re.match (r'echo:  M218 T1 X(-?[0-9]{1,3}\.[0-9][0-9]) Y(-?[0-9]{1,3}\.[0-9][0-9]) Z(-?[0-9]{1,3}\.[0-9][0-9])', responseLine)
+			if matcher:
+				m218X = matcher.group(1) 
+				m218Y = matcher.group(2) 
+				m218Z = matcher.group(3) 
 
 
-	elif data_raw.startswith("echo:  M851") :
-		matcher = re.match (r'echo:  M851 Z(-?[0-9]\.[0-9][0-9])', responseLine)
-		if matcher:
-			m851Z = matcher.group(1) 
+		elif data_raw.startswith("echo:  M851") :
+			matcher = re.match (r'echo:  M851 Z(-?[0-9]\.[0-9][0-9])', responseLine)
+			if matcher:
+				m851Z = matcher.group(1) 
 
 
 if m206X != None and  m206Y != None and  m206Z != None  :
@@ -214,7 +218,12 @@ else:
 
 	sys.stdout.flush()
 
-if int(extruderCount) == 2:
+
+if extruderCount == None:
+	print ("Could not read number of extruders - Please proceed to Quick Check after firmware has uploaded")
+	subprocess.check_output('echo Could not read number of extruders - Please proceed to Quick Check after firmware has uploaded >> /home/pi/.octoprint/logs/firmware.log',shell=True)
+
+elif int(extruderCount) == 2:
 
 	if m218X != None and  m218Y != None and  m218Z != None  :
 		m218String = "M218 T1 X" + m218X + " Y" + m218Y + " Z" + m218Z + "\n\r"
@@ -223,13 +232,11 @@ if int(extruderCount) == 2:
 		subprocess.check_output('echo Could not read previous M503 M218 - Please proceed to Quick Check after firmware has uploaded >> /home/pi/.octoprint/logs/firmware.log',shell=True)
 
 		sys.stdout.flush()
-elif int(extruderCount) == None:
-	print ("Could not read number of extruders - Please proceed to Quick Check after firmware has uploaded")
-	subprocess.check_output('echo Could not read number of extruders - Please proceed to Quick Check after firmware has uploaded >> /home/pi/.octoprint/logs/firmware.log',shell=True)
 
+if zprobe == None:
+	print ("Could not read previous M503 M851 - Please proceed to Quick Check after firmware has uploaded")
 
-
-if int(zprobe) == 1:
+elif int(zprobe) == 1:
 	if m851Z != None  :
 		sys.stdout.flush()
 		m851String = "M851 Z" + m851Z + "\n\r"
@@ -267,7 +274,7 @@ sys.stdout.flush()
 
 try:
 	subprocess.check_output('/home/pi/.platformio/packages/tool-avrdude/avrdude -cwiring -p atmega2560 -P/dev/ttyS0 -b115200 -D -Uflash:w:.pioenvs/megaatmega2560/firmware.hex >> /home/pi/.octoprint/logs/firmware.log',shell=True)
-	print "Upload Sucessful "
+	print "Upload Successful "
 	subprocess.check_output('echo Upload Sucessful >> /home/pi/.octoprint/logs/firmware.log',shell=True)
 	sys.stdout.flush()
 except subprocess.CalledProcessError:
@@ -279,35 +286,36 @@ except subprocess.CalledProcessError:
 
 time.sleep(5)
 
-print "Restoring parameters "
-subprocess.check_output('echo Restoring parameters >> /home/pi/.octoprint/logs/firmware.log',shell=True)
-sys.stdout.flush()
+if matcher != None :
+	print "Restoring parameters "
+	subprocess.check_output('echo Restoring parameters >> /home/pi/.octoprint/logs/firmware.log',shell=True)
+	sys.stdout.flush()
 
 
-ser = serial.Serial('/dev/ttyS0', 115200, timeout=2, xonxoff=False, rtscts=False, dsrdtr=False) #start serial port to upload script
-ser.flushInput()	#clear any latent input
-ser.flushOutput()   #clear any latent output
+	ser = serial.Serial('/dev/ttyS0', 115200, timeout=2, xonxoff=False, rtscts=False, dsrdtr=False) #start serial port to upload script
+	ser.flushInput()	#clear any latent input
+	ser.flushOutput()   #clear any latent output
 
 
-if m206String != None:
-	ser.write(m206String)
-	subprocess.check_output("echo M206 X" + m206X + " Y" + m206Y + " Z" + m206Z + " >> /home/pi/.octoprint/logs/firmware.log",shell=True)
+	if m206String != None:
+		ser.write(m206String)
+		subprocess.check_output("echo M206 X" + m206X + " Y" + m206Y + " Z" + m206Z + " >> /home/pi/.octoprint/logs/firmware.log",shell=True)
 
-	#print "write M206"
-if m851String != None:
-	ser.write(m851String)
-	subprocess.check_output("echo M851 Z" + m851Z + " >> /home/pi/.octoprint/logs/firmware.log",shell=True)
-	#print "write M851"
+		#print "write M206"
+	if m851String != None:
+		ser.write(m851String)
+		subprocess.check_output("echo M851 Z" + m851Z + " >> /home/pi/.octoprint/logs/firmware.log",shell=True)
+		#print "write M851"
 
-if int(extruderCount) == 2:
-	if m218String != None:
-		ser.write(m218String)
-	subprocess.check_output("echo M218 T1 X" + m218X + " Y" + m218Y + " Z" + m218Z + " >> /home/pi/.octoprint/logs/firmware.log",shell=True)
-	#print "write M218"
+	if int(extruderCount) == 2:
+		if m218String != None:
+			ser.write(m218String)
+		subprocess.check_output("echo M218 T1 X" + m218X + " Y" + m218Y + " Z" + m218Z + " >> /home/pi/.octoprint/logs/firmware.log",shell=True)
+		#print "write M218"
 
 
 
-ser.write("M500\n\r") #save parameters
+	ser.write("M500\n\r") #save parameters
 
 
 
